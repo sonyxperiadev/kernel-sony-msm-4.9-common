8bc7e605b8df465e0eeb82bf799ae91ba1beba8d
MarijnS95 <marijns95@gmail.com>
2019-05-26 10:06:51 +0200

    KernelConfig: Check BUILD_KERNEL neq to true.
    Checking against the value being not equal to true prevents a situation where it's neither equal to true or false (unset, for example). This matches the ifeq to true in the inline kernel script.

--- KernelConfig.mk
+++ KernelConfig.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq ($(BUILD_KERNEL),false)
+ifneq ($(BUILD_KERNEL),true)
 
 LOCAL_KERNEL := $(KERNEL_PATH)/common-kernel/kernel-dtb-$(TARGET_DEVICE)
 
